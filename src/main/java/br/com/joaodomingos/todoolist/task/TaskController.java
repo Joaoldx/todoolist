ec5eada665f61341b076ddcc33a8fb67828fa807
João Domingos <j13domingos@gmail.com>
2023-10-14 20:12:37 -0300

    feat: Add method to check if there is null properties, and add posibility to change only part of task model

--- src/main/java/br/com/joaodomingos/todoolist/task/TaskController.java
+++ src/main/java/br/com/joaodomingos/todoolist/task/TaskController.java
@@ -9,13 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.joaodomingos.todoolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -46,20 +47,20 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
     
-    @GetMapping("/")
     public ResponseEntity<List<TaskModel>> list(HttpServletRequest servletRequest) {
         var idUser = servletRequest.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return ResponseEntity.status(HttpStatus.OK).body(tasks);
     }
     
-    @PutMapping("/{id}")
-    public ResponseEntity<TaskModel> update (@RequestBody TaskModel taskModel, HttpServletRequest servletRequest, @PathVariable UUID id) {
-        var idUser = servletRequest.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        this.taskRepository.save(taskModel);
-        return ResponseEntity.status(HttpStatus.OK).body(taskModel);
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskModel> update (@RequestBody TaskModel taskModel, HttpServletRequest servletRequest, @PathVariable UUID id) {        
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
     
     @DeleteMapping("/{id}")

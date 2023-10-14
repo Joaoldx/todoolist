a11f0eab75e4b1ca2d7af60e69a595a2dfe91f08
João Domingos <j13domingos@gmail.com>
2023-10-14 01:53:56 -0300

    feat: Created Java app with user endpoint

---
+++ src/main/java/br/com/joaodomingos/todoolist/TodoolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaodomingos.todoolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoolistApplication.class, args);
+	}
+
+}
